a1e4b590b9346172e75db80df19e55d99b2336c4
Philippe Warnon <phw@civadis.be>
2019-06-12 18:01:59 +0200

    first commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: './srv/region/region.module#SrvRegionModule'
+      },
+      {
+        path: 'country',
+        loadChildren: './srv/country/country.module#SrvCountryModule'
+      },
+      {
+        path: 'location',
+        loadChildren: './srv/location/location.module#SrvLocationModule'
+      },
+      {
+        path: 'department',
+        loadChildren: './srv/department/department.module#SrvDepartmentModule'
+      },
+      {
+        path: 'task',
+        loadChildren: './srv/task/task.module#SrvTaskModule'
+      },
+      {
+        path: 'employee',
+        loadChildren: './srv/employee/employee.module#SrvEmployeeModule'
+      },
+      {
+        path: 'job',
+        loadChildren: './srv/job/job.module#SrvJobModule'
+      },
+      {
+        path: 'job-history',
+        loadChildren: './srv/job-history/job-history.module#SrvJobHistoryModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
